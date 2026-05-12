50728656dc3d780550da9c9fc66dd62c839ef86a
kori756 <khorirachmatr@gmail.com>
2026-05-12 19:18:26 +0700

    public untuk class orang

--- inheritance.cpp
+++ inheritance.cpp
@@ -3,3 +3,24 @@
 using namespace std;
 
 class orang
+{ // asosiasi
+public:
+    string nama;
+
+    orang(string pNama) : nama(pNama)
+    {
+        cout << "orang dibuat \n"
+             << endl;
+    }
+    ~orang()
+    {
+        cout << "orang dihapus\n"
+             << endl;
+    }
+
+    int jumlah(int a, int b)
+    {
+        return a + b;
+    }
+};
+
