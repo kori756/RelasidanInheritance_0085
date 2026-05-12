fc22f4f420044d96eb01977d75b6195c597dc063
kori756 <khorirachmatr@gmail.com>
2026-05-12 19:11:53 +0700

    asosiasi untuk class pasien

--- asociation.cpp
+++ asociation.cpp
@@ -6,3 +6,18 @@ using namespace std;
 
 class dokter;
 class pasien
+{ // asosiasi
+public:
+    string nama;
+    vector<dokter *> daftar_dokter;
+    pasien(string pNama) : nama(pNama)
+    {
+        cout << "pasien \"" << nama << "\" ada \n";
+    }
+    ~pasien()
+    {
+        cout << "pasien \"" << nama << "\" tidak ada \n";
+    }
+    void tambahDokter(dokter *);
+    void cetakDokter();
+};
